5d36ee538cea3b434053eac3e6a57227f69e1584
Scott Culp <scot.r.culp@hotmail.com>
2019-03-13 18:46:57 -0500

    change 1

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+void addPlusPlus (ifstream& inStream, ofstream& outStream);
+
+int main ()
+{
+	ifstream fin;
+	ofstream fout;
+	cout << "Begin counting letters.\n";
+	fin.open("info.dat");
+	
+
+	
+
+}
